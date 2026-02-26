f55366e0ac5d5db5d1a0c9170230e64d5ce12eb5
tengo-flow <cs5100@proton.me>
2026-02-26 08:09:16 -0500

    Create core.clj
    code

---
+++ src/clojure_noob/core.clj
@@ -0,0 +1,12 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "the are the little teapots"))
+(println "Clreanliness i bestliness")
+
+(defn fish
+  []
+  (println "glug glug!!"))
